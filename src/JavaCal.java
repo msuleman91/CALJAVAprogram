cc9dd600e465dd045fde3cd230c2a176cbe35831
msuleman91 <msuleman387@gmail.com>
2019-05-08 11:46:54 -0700

    second commit on master

--- src/JavaCal.java
+++ src/JavaCal.java
@@ -6,11 +6,17 @@ public class JavaCal {
 		
 		return a+b;
 	}
+	public int sub(int a, int b)
+	{
+		
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		JavaCal cal=new JavaCal();
 		
 		System.out.println(cal.sum(10, 10));
+		System.out.println(cal.sub(10, 8));
 
 	}
 

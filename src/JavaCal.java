fc85cc54193e3aca7d08f4ab14ced6d1e2a337ce
msuleman91 <msuleman387@gmail.com>
2019-05-08 11:52:28 -0700

    first commit in featurebranch

--- src/JavaCal.java
+++ src/JavaCal.java
@@ -11,12 +11,19 @@ public class JavaCal {
 		
 		return a-b;
 	}
+	
+	public int mul(int a, int b)
+	{
+		
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		JavaCal cal=new JavaCal();
 		
 		System.out.println(cal.sum(10, 10));
 		System.out.println(cal.sub(10, 8));
+		System.out.println(cal.mul(10, 8));
 
 	}
 
